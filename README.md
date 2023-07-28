16f400e5fd06fa83297ac6364489c46d21e06c3a
Balázs Gerlei <gerlotdev@gmail.com>
2023-07-28 09:12:36 +0200

    Initial commit. Add first version of the sample app.

---
+++ README.md
@@ -0,0 +1,2 @@
+# Android Biometric Class Detector
+Sample app to display capabilities of Android devices that can be used in a BiometricPrompt 
---
+++ app/src/main/java/com/example/biometricclassdetector/BiometricProperties.kt
@@ -0,0 +1,20 @@
+package com.example.biometricclassdetector
+
+enum class BiometricType {
+    FINGERPRINT, CAMERA, IRIS
+}
+
+enum class BiometricClass {
+    CLASS1, CLASS2, CLASS3
+}
+
+data class BiometricClassDetails(
+    val biometricClass: BiometricClass,
+    val enrolled: Boolean,
+)
+
+data class BiometricProperties(
+    val isDeviceSecure: Boolean,
+    val availableBiometricTypes:  List<BiometricType>,
+    val availableBiometricClasses: List<BiometricClassDetails>,
+)
