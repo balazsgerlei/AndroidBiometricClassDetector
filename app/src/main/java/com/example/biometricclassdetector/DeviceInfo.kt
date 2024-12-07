06b826bbb0550920e72a83b37e63526013a073f9
Balázs Gerlei <gerlotdev@gmail.com>
2024-12-07 09:10:14 +0100

    Display basic device info (manufacturer, model and Android version) on screen
    - Make Previews more realistic with showing Class 2 as well as Class 3 (a Class 3 sensor can always work as Class 2)

---
+++ app/src/main/java/com/example/biometricclassdetector/DeviceInfo.kt
@@ -0,0 +1,9 @@
+package com.example.biometricclassdetector
+
+data class DeviceInfo(
+    val deviceName: String,
+    val deviceBrand: String,
+    val deviceModel: String,
+    val androidVersion: String,
+    val androidApiLevel: Int,
+)
